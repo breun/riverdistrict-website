e5d62f9917e08c433726c645339593a0070d370f
Nils Breunese <nils@breun.nl>
2014-04-02 02:10:19 +0200

    Laat de compress plugin de presskit zip maar maken.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,14 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        copy: {
+	    main: {
+	        expand: true,
+	        cwd: 'static/',
+	        src: '**',
+		dest: 'dest/'
+	    }
+	},
         cssmin : {
             css: {
                 src: 'css/*.css',
@@ -14,15 +22,21 @@ module.exports = function (grunt) {
 		dest: 'dest/combined.min.js'
             }
         },
-	copy: {
-	    main: {
+	compress: {
+	    presskit: {
+	        options: {
+		    archive: 'dest/riverdistrict-presskit.zip'
+		},
 	        expand: true,
-	        cwd: 'static/',
-	        src: '**',
-		dest: 'dest/'
+		cwd: 'presskit/',
+		src: '**'
 	    }
 	},
         watch: {
+            presskit: {
+                files: 'presskit/**',
+                tasks: 'compress'
+            },
 	    css: {
 	        files: 'css/*.css',
 		tasks: 'cssmin'
@@ -38,10 +52,11 @@ module.exports = function (grunt) {
         }
     });
 
+    grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['copy', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['copy', 'cssmin', 'uglify', 'compress']);
 };
